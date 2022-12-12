49678d1141a003382dd24919093d3d3f06e632c6
salar403 <salar40340@gmail.com>
2022-12-13 02:08:03 +0330

    added rabbitmq basic config and producer/consumers

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     used_traffic = models.BigIntegerField(null=False, default=0)
 
 class V2rayClient(models.Model):
-    uuid = models.CharField(null=False)
+    uuid = models.CharField(null=False, max_length=50)
     name = models.CharField(max_length=1000, null=False)
     user = models.OneToOneField(
         to=User,
